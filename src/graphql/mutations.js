2e87e4df99828333ab3c4924f8672cc17911c1ed
DrArtemi <adrien.milcent@outlook.com>
2021-03-11 17:32:42 +0100

    edit mutations to accept status and message attributes

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -3,14 +3,18 @@ import gql from 'graphql-tag'
 export const LOGIN_USER = gql`
 mutation login ($email: String! $password: String! ){
   login(email: $email password: $password ){
-    token
+    token,
+    status,
+    message
   }
 }
 `
 export const REGISTER_USER = gql`
 mutation registerUser($pseudo: String! $email: String! $password: String! ) {
     registerUser( pseudo: $pseudo, email: $email, password: $password) {
-      token
+      token,
+      status,
+      message
     }
 }
 `
