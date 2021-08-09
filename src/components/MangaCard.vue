3ea9fdb113ef4c4c65e304c9921c3d721b1ea3e9
DrArtemi <adrien.milcent@outlook.com>
2021-08-09 19:30:07 +0200

    fix missing crochet

--- src/components/MangaCard.vue
+++ src/components/MangaCard.vue
@@ -5,7 +5,7 @@
         :style="[manga.cover_path ?
                 { backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)), url(/manga_covers/' + manga.cover_path + ')' } :
                 { backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)), rgba(31, 41, 55, 1)' }
-                "
+                ]"
     >
         <div class="self-end ml-2 mb-1">
             <h3 class="text-base font-bold text-white">{{ manga.title }}</h3>
