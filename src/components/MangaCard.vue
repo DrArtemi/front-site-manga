d02758e3a917dc1c78a06b2695df59b4bf0a2733
DrArtemi <adrien.milcent@outlook.com>
2021-08-09 19:25:11 +0200

    add background if cover image doesn't exist

--- src/components/MangaCard.vue
+++ src/components/MangaCard.vue
@@ -2,7 +2,10 @@
     <a 
         :href="'#'"
         class="flex w-48 h-56 mr-2 bg-cover bg-center align-text-bottom rounded cursor-pointer"
-        :style="{ backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)), url(/manga_covers/' + manga.cover_path + ')'}"
+        :style="[manga.cover_path ?
+                { backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)), url(/manga_covers/' + manga.cover_path + ')' } :
+                { backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)), rgba(31, 41, 55, 1)' }
+                "
     >
         <div class="self-end ml-2 mb-1">
             <h3 class="text-base font-bold text-white">{{ manga.title }}</h3>
