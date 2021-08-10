2bf17e2f4a6b1a9dd275a420d51212f27e0c6716
DrArtemi <adrien.milcent@outlook.com>
2021-08-10 18:43:09 +0200

    add manga url field

--- src/components/MangaCard.vue
+++ src/components/MangaCard.vue
@@ -1,6 +1,6 @@
 <template>
     <a 
-        :href="'#'"
+        :href="manga.url" target="_blank"
         class="flex w-48 h-56 mr-2 bg-cover bg-center align-text-bottom rounded cursor-pointer"
         :style="[manga.cover_path ?
                 { backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)), url(/manga_covers/' + manga.cover_path + ')' } :
