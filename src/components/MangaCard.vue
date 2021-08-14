e50e563001fd8f2ad397dcb8a40400b7889a6b96
DrArtemi <adrien.milcent@outlook.com>
2021-08-14 02:04:22 +0200

    Add login + favorite slider

--- src/components/MangaCard.vue
+++ src/components/MangaCard.vue
@@ -1,25 +1,41 @@
 <template>
     <a 
         :href="manga.url" target="_blank"
-        class="flex w-32 md:w-40 xl:w-48 h-40 md:h-48 xl:h-56 mr-2 bg-cover bg-center align-text-bottom rounded cursor-pointer"
+        class="flex flex-col w-32 md:w-40 xl:w-48 h-40 md:h-48 xl:h-56 mr-2 bg-cover bg-center align-text-bottom rounded cursor-pointer"
         :style="[manga.cover_path ?
                 { backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)), url(/manga_covers/' + manga.cover_path + ')' } :
                 { backgroundImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.8)), rgba(31, 41, 55, 1)' }
                 ]"
     >
-        <div class="self-end ml-2 mb-1">
-            <h3 class="text-base font-bold text-white">{{ manga.title }}</h3>
+        <div class="flex ml-2 mt-auto">
+            <h3 class="text-base font-bold text-white mb-1 self-end">{{ manga.title }}</h3>
+            <LikeButton
+                v-if="authStatus"
+                class="ml-auto self-end"
+                likeType="manga"
+                :likeId="index"
+                :mangaId="manga.id"
+            />
         </div>
     </a>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import LikeButton from '../components/LikeButton.vue'
 
 export default {
     name: 'MangaCard',
+    components: {
+        LikeButton,
+    },
     props: [
         'manga',
+        'index'
     ],
+    computed: {
+        ...mapGetters(['authStatus']),
+    }
 }
 
 </script>
