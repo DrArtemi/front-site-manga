aa62addeae887bfa1e050c075e6f47b5a142399a
DrArtemi <adrien.milcent@outlook.com>
2021-08-29 10:12:34 +0200

    Add style to login and register errors

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -18,15 +18,54 @@
                                 <div class="grid grid-cols-6 gap-6">
                                     <div class="col-span-6 sm:col-span-4">
                                         <label for="pseudo" class="block text-sm font-medium text-gray-200">Identifiant</label>
-                                        <input type="text" name="pseudo"  placeholder="TomAto" v-model="authDetails.pseudo" class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                                        <input 
+                                            type="text"
+                                            name="pseudo"
+                                            placeholder="TomAto"
+                                            v-model="authDetails.pseudo"
+                                            class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            :class="{ 'border-red-500': pseudoFailure !== '' }"
+                                        />
+                                        <p
+                                            v-if="pseudoFailure !== ''"
+                                            class="text-red-500 m-0"
+                                        >
+                                            {{ pseudoFailure }}
+                                        </p>
                                     </div>
                                     <div class="col-span-6 sm:col-span-4">
                                         <label for="email" class="block text-sm font-medium text-gray-200">Adresse email</label>
-                                        <input type="email" name="email"  placeholder="exemple@email.com" v-model="authDetails.email" class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                                        <input 
+                                            type="email"
+                                            name="email" 
+                                            placeholder="exemple@email.com"
+                                            v-model="authDetails.email"
+                                            class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            :class="{ 'border-red-500': emailFailure !== '' }"
+                                        />
+                                        <p
+                                            v-if="emailFailure !== ''"
+                                            class="text-red-500 m-0"
+                                        >
+                                            {{ emailFailure }}
+                                        </p>
                                     </div>
                                     <div class="col-span-6 sm:col-span-4">
                                         <label for="password" class="block text-sm font-medium text-gray-200">Mot de passe</label>
-                                        <input type="password" name="password" placeholder="mot de passe" v-model="authDetails.password" class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+                                        <input 
+                                            type="password"
+                                            name="password"
+                                            placeholder="mot de passe"
+                                            v-model="authDetails.password"
+                                            class="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                            :class="{ 'border-red-500': passwordFailure !== '' }"
+                                        />
+                                        <p
+                                            v-if="passwordFailure !== ''"
+                                            class="text-red-500 m-0"
+                                        >
+                                            {{ passwordFailure }}
+                                        </p>
                                     </div>
                                 </div>
                             </div>
@@ -63,14 +102,35 @@ export default {
                 pseudo: '',
                 email: '',
                 password: ''
-            }
+            },
+            pseudoFailure: '',
+            emailFailure: '',
+            passwordFailure: '',
         }
     },
     methods: {
         ...mapActions(['register']),
 
         registerUser: function () {
-            this.register(this.authDetails).then(() => this.$router.push('/'));
+            var self = this;
+            this.pseudoFailure = '';
+            this.emailFailure = '';
+            this.passwordFailure = '';
+            this.register(this.authDetails).then(function(res) {
+                if (res.status === 'success')
+                    self.$router.push('/');
+                else {
+                    let failRes = res.message.split(':');
+                    if (failRes.length == 2) {
+                        if (failRes[0] === 'pseudo')
+                            self.pseudoFailure = failRes[1];
+                        else if (failRes[0] === 'mail')
+                            self.emailFailure = failRes[1];
+                        else if (failRes[0] === 'password')
+                            self.passwordFailure = failRes[1];
+                    }
+                }
+            });
         }
     }
 }
